89c68cb6f77511e53e3fdfbc3f070caff8532f62
Prabal Deshar <prabaldeshar@gmail.com>
2025-02-27 12:01:39 +0545

    remove: branch variable from bulild and deploy uat

--- statment-analysis-be/api/utils/datetime_utils.py
+++
@@ -1,6 +0,0 @@
-from datetime import datetime
-
-
-def convert_datetime_str_to_datetime(datetime_str: str) -> datetime:
-    datetime_obj = datetime.strptime(datetime_str, "%Y-%m-%d %H:%M:%S")
-    return datetime_obj
--- statment-analysis-be/shell.py
+++
@@ -1,9 +0,0 @@
-import ipdb
-from sqlmodel import Session
-
-from api.db.core import get_session
-
-db: Session = next(get_session())
-
-print("FastAPI Shell - Database Session Loaded!")
-ipdb.set_trace()

735cf654120c80a48308e6926d85a15471e7f5f5
Prabal Deshar <prabaldeshar@gmail.com>
2025-02-27 09:49:21 +0545

    feat: add type hints and docstrings to API endpoints for better clarity

--- api/routers/filter.py
+++ api/routers/filter.py
@@ -2,7 +2,7 @@ from fastapi import APIRouter, Depends, Request
 from sqlmodel import Session
 
 from api.db.core import get_session
-from api.db.transactions import get_filter_fields
+from api.db.transactions import FilterField, get_filter_fields
 
 router = APIRouter(
     prefix="/filter-fields",
@@ -10,6 +10,9 @@ router = APIRouter(
 
 
 @router.get("/")
-async def get_filters(request: Request, db: Session = Depends(get_session)):
+async def get_filters(request: Request, db: Session = Depends(get_session)) -> FilterField:
+    """
+        Retrive the table fields(columns) which be used as filters in the Frontend table
+    """
     filter_fields = get_filter_fields(db)
     return filter_fields
